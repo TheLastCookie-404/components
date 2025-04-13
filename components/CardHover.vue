951214ec4e32206ddddd9d3049fa28d6c4286628
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-04-13 20:44:58 +0500

    Component class exposed

--- components/CardHover.vue
+++ components/CardHover.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative box-content overflow-hidden base-block"
+    class="relative overflow-hidden base-block"
     :style="`padding: ${borderWidth}`"
     @mousemove="handleMouse"
     @mouseleave="isMouseLeave = true">
@@ -20,72 +20,72 @@
 </template>
 
 <script setup>
-const mouseX = ref(0);
-const mouseY = ref(0);
-const isMouseLeave = ref(true);
+  const mouseX = ref(0);
+  const mouseY = ref(0);
+  const isMouseLeave = ref(true);
 
-defineProps({
-  lightColor: {
-    type: String,
-    required: false,
-    default: "#fff",
-  },
-  lightBlur: {
-    type: String,
-    required: false,
-    default: "40px",
-  },
-  baseColor: {
-    type: String,
-    required: false,
-    default: "#15191e",
-  },
-  containerColor: {
-    type: String,
-    required: false,
-    default: "#191e24f8",
-  },
-  borderWidth: {
-    type: String,
-    required: false,
-    default: "2px",
-  },
-  borderRadius: {
-    type: String,
-    required: false,
-    default: "0",
-    validator: (value) => {
-      return /^\d+(\.\d+)?(px|rem|em|%)$/.test(value);
+  defineProps({
+    lightColor: {
+      type: String,
+      required: false,
+      default: "#fff",
     },
-  },
-});
+    lightBlur: {
+      type: String,
+      required: false,
+      default: "40px",
+    },
+    baseColor: {
+      type: String,
+      required: false,
+      default: "#15191e",
+    },
+    containerColor: {
+      type: String,
+      required: false,
+      default: "#191e24f8",
+    },
+    borderWidth: {
+      type: String,
+      required: false,
+      default: "2px",
+    },
+    borderRadius: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: (value) => {
+        return /^\d+(\.\d+)?(px|rem|em|%)$/.test(value);
+      },
+    },
+  });
 
-function handleMouse(event) {
-  isMouseLeave.value = false;
-  mouseX.value = event.offsetX;
-  mouseY.value = event.offsetY;
-}
+  function handleMouse(event) {
+    isMouseLeave.value = false;
+    mouseX.value = event.offsetX;
+    mouseY.value = event.offsetY;
+  }
 </script>
 
 <style lang="css" scoped>
-.base-block {
-  background: v-bind(baseColor);
-  border-radius: v-bind(borderRadius);
-}
+  .base-block {
+    background: v-bind(baseColor);
+    border-radius: v-bind(borderRadius);
+  }
 
-.light-sircle {
-  background: v-bind(lightColor);
-  filter: blur(v-bind(lightBlur));
-}
+  .light-sircle {
+    background: v-bind(lightColor);
+    filter: blur(v-bind(lightBlur));
+  }
 
-.content-container {
-  background: v-bind(containerColor);
-  border-radius: calc(v-bind(borderRadius) - v-bind(borderWidth));
-}
+  .content-container {
+    background: v-bind(containerColor);
+    border-radius: calc(v-bind(borderRadius) - v-bind(borderWidth));
+  }
 
-.mouse-pos {
-  position: absolute;
-  top: v-bind("`${mouseY}px`");
-  left: v-bind("`${mouseX}px`");
-}
+  .mouse-pos {
+    position: absolute;
+    top: v-bind("`${mouseY}px`");
+    left: v-bind("`${mouseX}px`");
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="p-5">
     <CardHover class="w-64 h-32 p-10" borderRadius="12px" lightColor="red">
-      <div class="mt-10">Lol</div>
+      <div class="p-5">Lol</div>
     </CardHover>
+    <br />
+    <Test />
   </div>
 </template>
